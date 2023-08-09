4cfb64cf898bd18c33e319b72da9a306ce104b3e
dellins <oyeyemidellins@gmail.com>
2023-08-09 14:46:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.title("Template Co-pilot")
 title = st.text_input('Type command here', 'Generate a group template for a consignment type with 1 list and 1 linked-list and 1 heirarchy')
 
 st.button('Generate')
-
+st.divider()
 body = """
 templateName: consignment_group_template
 templateType: group
