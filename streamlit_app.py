e9fb1f4fb01896d6bcbf9aa11d4a623f39ce65fe
dellins <oyeyemidellins@gmail.com>
2023-08-09 14:57:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,52 +2,33 @@ import requests
 import json
 import streamlit as st
 
+def consume_url(message, history):
+    url = "https://y9s346bpuj.execute-api.us-east-1.amazonaws.com/default/bedrock_opensearch_demo"
+    body = {
+        "message": message,
+        "history": history,
+        "action": 'template'
+    }
+    headers = {'Content-Type': 'application/json', 'x-api-key': 'LF1LAK6AKF9Zk1gJMRWKZ5ERJ2oMraAf6V58owSt'}
+    response = requests.post(url, headers=headers, data=json.dumps(body))
+    
+    if response.status_code == 200:
+        data = response.json()
+
+        # Process the data as needed
+        print(data)
+
+        return data
+    else:
+        print("Failed to fetch data. Status code:", response.status_code)
 
 st.title("Template Co-pilot")
 
 title = st.text_input('Type command here', 'Generate a group template for a consignment type with 1 list and 1 linked-list and 1 heirarchy')
 
+body = consume_url(title, "")
+
 st.button('Generate')
 st.divider()
-body = """
-templateName: consignment_group_template
-templateType: group
 
-type: consignment
-description: consignment of items
-referenceType: CBR
-referenceSuffix: Z
-
-data:
-  - name: linked-list
-    lookUpId: 1
-    title: A title for the linked list
-    reference:
-      templateName: linked_list_1_consignment_template
-      templateType: linked_list
-  - name: linked-list
-    lookUpId: 2
-    title: Another title for the linked list
-    subTitle: And an optional sub title
-    reference:
-      templateName: linked_list_2_consignment_template
-      templateType: linked_list
-  - name: list
-    lookUpId: 1
-    title: A title for list
-    description: >
-      A long, optional description that can be added to help explain.
-    reference:
-      templateName: list_consignment_template
-      templateType: list
-  - name: hierarchy
-    lookUpId: 1
-    title: A title fro the hierarchy
-    subTitle: A sub title for the hierarchy.
-    description: >
-      A descriptive body of text to explain more about it.
-    reference:
-      templateName: hierarchy_template
-      templateType: hierarchy
-"""
 st.code(body, language="yaml", line_numbers=False)
